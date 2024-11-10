6f11f5f843c5f9c9c84c958e3cf389d94bacb1ba
Machiel Brinkman <machielbrinkman@outlook.com>
2024-11-10 01:00:10 +0100

    Added script module and PowerShell data file

---
+++ WinPE/WinPE.psm1
@@ -0,0 +1,20 @@
+
+# Requires -RunAsAdministrator
+
+# Import Functions
+. $PSScriptRoot\Functions\Private\HelperFunctions.ps1
+. $PSScriptRoot\Functions\Public\Copy-WinPE.ps1
+. $PSScriptRoot\Functions\Public\Mount-WinPEBootImage.ps1
+. $PSScriptRoot\Functions\Public\Dismount-WinPEBootImage.ps1
+. $PSScriptRoot\Functions\Public\Add-WinPEOptionalComponent.ps1
+#. $PSScriptRoot\Functions\Public\Update-WinPE.ps1
+#. $PSScriptRoot\Functions\Public\New-WinPEMedia.ps1
+
+#HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows Kits\Installed Roots
+# Requires .\adksetup.exe /ceip off /features OptionId.WindowsPreinstallationEnvironment /quiet
+#. $PSScriptRoot\Functions\Install-WindowsADK.ps1
+
+New-Alias -Name copype -Value Copy-WinPE -Description "Creates working directories for WinPE image customization and media creation."
+#New-Alias -Name MakeWinPEMedia -Value New-WinPEMEdia -Description "Creates bootable WinPE USB flash drive or ISO file."
+
+Export-ModuleMember -Function * -Alias *

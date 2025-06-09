a1bf33a2d423e3aad56a39bdde2906719d62d7d3
Machiel Brinkman <machielbrinkman@outlook.com>
2025-06-09 11:30:40 +0200

    Replaced Find-ADKInstallation with Find-WinPEInstallation

--- WinPE/Functions/Private/HelperFunctions.ps1
+++ WinPE/Functions/Private/HelperFunctions.ps1
@@ -1,6 +1,6 @@
 <# 
  .SYNOPSIS
-    Find-ADKInstallation...
+    Find-WinPEInstallation...
 
  .DESCRIPTION
     ...
@@ -10,10 +10,14 @@
 
 #>
 
-function Find-ADKInstallation
+function Find-WinPEInstallation
 {
-
-    $ADKInstallationPath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows Kits\Installed Roots' -Name KitsRoot10
-    return $ADKInstallationPath
-
+   # Find if the 'Kits Configuration Installer' is installed, then the Windows ADK WinPE Add-ons might be installed
+   $KitsConfigurationInstaller = Get-CimInstance -ClassName Win32_Product | Where-Object {$_.Name -eq 'Kits Configuration Installer'}
+   if ($KitsConfigurationInstaller)
+   {
+      $WinPE.Version = $KitsConfigurationInstaller.Version
+      $WinPE.InstallPath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows Kits\Installed Roots' -Name KitsRoot10
+      Get-ItemPropertyValue -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows Kits\Installed Roots\$version\Installed Options" -Name OptionId.WindowsPreinstallationEnvironment
+   }
 }

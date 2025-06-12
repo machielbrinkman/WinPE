dee79ae665a75813e7a7a8f3e1f9258f79cf3411
Machiel Brinkman <machielbrinkman@outlook.com>
2025-06-13 00:29:21 +0200

    Added Parameterssets

--- WinPE/Functions/Public/Dismount-WinPEBootImage.ps1
+++ WinPE/Functions/Public/Dismount-WinPEBootImage.ps1
@@ -12,19 +12,26 @@
 
 function Dismount-WinPEBootImage
 {
-    [CmdletBinding()]
+    [CmdletBinding(DefaultParameterSetName = 'Save')]
     Param
     (
         # Specifies the Windows PE Architecture to use.
         # Valid values are: amd64, x86, arm or arm64.
-        [Parameter(Mandatory = $true, Position = 0)]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Save', Position = 0)]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Discard', Position = 0)]
         [ValidateSet('amd64', 'x86', 'arm', 'arm64')]
         [string]$WinPEArch,
 
         # Creates the working directory at the specified location.
-        [Parameter(Mandatory = $true, Position = 1)]
-        [string]$Path
+        [Parameter(Mandatory = $true, ParameterSetName = 'Save', Position = 1)]
+        [Parameter(Mandatory = $true, ParameterSetName = 'Discard', Position = 1)]
+        [string]$Path,
 
+        [Parameter(Mandatory = $true, ParameterSetName = 'Save', Position = 2)]
+        $Save,
+
+        [Parameter(Mandatory = $true, ParameterSetName = 'Discard', Position = 2)]
+        $Discard
     )
 
     # Dismount the WinPE Boot Image

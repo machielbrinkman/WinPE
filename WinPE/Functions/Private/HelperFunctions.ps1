65e90491e9d72c641792fa4a321b1a8576bebbfa
Machiel Brinkman <machielbrinkman@outlook.com>
2025-06-17 01:25:51 +0200

    Minor comment changes

--- WinPE/Functions/Private/HelperFunctions.ps1
+++ WinPE/Functions/Private/HelperFunctions.ps1
@@ -3,12 +3,12 @@
    Search for the WinPE Installation and collect some properties of it.
 
  .DESCRIPTION
-   Search if the 'Kits Configuration Installer' is installed, which is the installer for the Windows ADK and the Windows PE Add-ons.
-   Next look if the Windows ADK WinPE Add-ons is installed and collect the necessary properties.
+   Search if the 'Kits Configuration Installer' is installed, which is the installer for the Windows ADK and the Windows PE Add-on.
+   Next look if the Windows ADK WinPE Add-on is installed and collect the necessary properties.
  #>
 function Find-WinPEInstallation
 {
-   # Search if the 'Kits Configuration Installer' is installed, next if the Windows ADK WinPE Add-ons are installed
+   # Search if the 'Kits Configuration Installer' is installed, next if the Windows ADK WinPE Add-on is installed
    $KitsConfigurationInstaller = Get-CimInstance -ClassName Win32_Product | Where-Object {$_.Name -eq 'Kits Configuration Installer'}
    if ($KitsConfigurationInstaller)
    {
@@ -18,7 +18,7 @@ function Find-WinPEInstallation
       # The Kits Configuration version somehow uses always 10.0.xxxx.0
       $KitsConfigurationVersion = $WinPE.Version -replace "^10\.1", "10.0" -replace "\.[^.]+$", ".0"
       
-      # Determine if the Windows PE Add-ons are installed
+      # Determine if the Windows PE Add-on is installed
       $WinPEOptionInstalled = Get-ItemProperty -Path "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows Kits\Installed Roots\$KitsConfigurationVersion\Installed Options" -Name OptionId.WindowsPreinstallationEnvironment -ErrorAction SilentlyContinue
       if ($WinPEOptionInstalled)
       {
